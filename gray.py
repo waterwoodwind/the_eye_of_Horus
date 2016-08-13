8a78e14b1400654cf92ccdf9df7de9bd63e1b781
waterwindwood <469798015@qq.com>
2016-08-13 19:15:25 +0800

    加入主函数

--- gray.py
+++ gray.py
@@ -117,78 +117,79 @@ class Pre_treat(object):
                 y_end_list.pop(i)
         return y_start_list, y_end_list
 
-clear_dir()
-character_list = []
-img_dir = "multi_img"
-#img_dir = "single_img"
-for img_file in os.listdir(img_dir):
-    img_path = os.path.join(img_dir, img_file)
-    img_name = img_file[:-4]
-    print img_path
-    thresh_img, data_gray_img = Pre_treat().get_data_thresh_img(img_path)
-    img_thresh_flt = thresh_img[:, two_flt_width_start:two_flt_width_end + 1]
-    img_thresh_plan_arrive = thresh_img[:, two_plan_lanch_start:two_plan_lanch_end+1]
-    data_flt = data_gray_img[:, two_flt_width_start:two_flt_width_end + 1]
-    data_plan_arrive = data_gray_img[:, two_plan_lanch_start:two_plan_lanch_end+1]
-    #避免边缘的锯齿，缩小边缘
-    img_index = thresh_img[:, two_index_start:two_index_end +1]
-    
-    y_start_list, y_end_list, x_start_list, x_end_list = \
-    Pre_treat().y_x_border_list(img_index)
-    
-    y_start_list, y_end_list = \
-    Pre_treat().del_surplus_y_line(y_start_list, y_end_list)
-    
-    #加入第一行蓝色区域
-    y_start_list.insert(0, 7)
-    y_end_list.insert(0, 19)
-    
-    #对第一行蓝色区域进行反色处理
-    ret,thresh_img[7-3:19+1+3,:] = cv2.threshold(thresh_img[7-3:19+1+3,:],254,255,cv2.THRESH_BINARY_INV)
-    
-    for i in range(len(y_start_list)):
-        line = img_thresh_flt[y_start_list[i]-3:y_end_list[i]+1+3,:]
-        line_thresh_arrive = img_thresh_plan_arrive[y_start_list[i]-3:y_end_list[i]+1+3,:]
+if __name__ == '__main__':
+    clear_dir()
+    character_list = []
+    img_dir = "multi_img"
+    #img_dir = "single_img"
+    for img_file in os.listdir(img_dir):
+        img_path = os.path.join(img_dir, img_file)
+        img_name = img_file[:-4]
+        print img_path
+        thresh_img, data_gray_img = Pre_treat().get_data_thresh_img(img_path)
+        img_thresh_flt = thresh_img[:, two_flt_width_start:two_flt_width_end + 1]
+        img_thresh_plan_arrive = thresh_img[:, two_plan_lanch_start:two_plan_lanch_end+1]
+        data_flt = data_gray_img[:, two_flt_width_start:two_flt_width_end + 1]
+        data_plan_arrive = data_gray_img[:, two_plan_lanch_start:two_plan_lanch_end+1]
+        #避免边缘的锯齿，缩小边缘
+        img_index = thresh_img[:, two_index_start:two_index_end +1]
         
-        line_color = data_flt[y_start_list[i]-3:y_end_list[i]+1+3,:]
-        line_data_plan_arrive = \
-        data_plan_arrive[y_start_list[i]-3:y_end_list[i]+1+3,:]
-        line_merge = np.hstack((line_color, line_data_plan_arrive))
-        cv2.imwrite('line/'+ img_name+ '_line_' + str(i) + '.bmp', line_merge)
+        y_start_list, y_end_list, x_start_list, x_end_list = \
+        Pre_treat().y_x_border_list(img_index)
         
+        y_start_list, y_end_list = \
+        Pre_treat().del_surplus_y_line(y_start_list, y_end_list)
         
-        for j in range(line_flt_number_start, line_flt_number_end + 1,character_width+1):
-            Character = line[:, j: j+character_width+1]
-            #cv2.imshow("Character_1", Character_1)
-            #cv2.waitKey(0)
-            if Character.sum()<>0:
-                cv2.imwrite('character/'+ img_name+ '_line_' + str(i) + '_' + str(j) + 'flt.tif', Character)
-                character_list.append(Character.tolist())
-                
-        for j in range(line_plane_number_start, line_plane_number_end + 1,character_width+1):
-            Character = line[:, j: j+character_width+1]
-            #cv2.imshow("Character_1", Character_1)
-            #cv2.waitKey(0)
-            if Character.sum()<>0:
-                cv2.imwrite('character/'+ img_name+ '_line_' + str(i) + '_' + str(j) + 'plane.tif', Character)
-                character_list.append(Character.tolist())
-                
-        for j in range(line_stand_start, line_stand_end + 1,character_width+1):
-            Character = line[:, j: j+character_width+1]
-            #cv2.imshow("Character_1", Character_1)
-            #cv2.waitKey(0)
-            if Character.sum()<>0:
-                cv2.imwrite('character/'+ img_name+ '_line_' + str(i) + '_' + str(j) + 'stand.tif', Character)
-                character_list.append(Character.tolist())
+        #加入第一行蓝色区域
+        y_start_list.insert(0, 7)
+        y_end_list.insert(0, 19)
         
-
-character_series = pd.Series(character_list)
-no_repeat = character_series.value_counts()
-for number,item in enumerate(no_repeat.index):
-    np_item = np.array(item)        
-    cv2.imwrite('no_repeat_character/' + \
-    str(no_repeat.iloc[number]) + '_' + \
-    str(number) + '.tif', np_item)
+        #对第一行蓝色区域进行反色处理
+        ret,thresh_img[7-3:19+1+3,:] = cv2.threshold(thresh_img[7-3:19+1+3,:],254,255,cv2.THRESH_BINARY_INV)
+        
+        for i in range(len(y_start_list)):
+            line = img_thresh_flt[y_start_list[i]-3:y_end_list[i]+1+3,:]
+            line_thresh_arrive = img_thresh_plan_arrive[y_start_list[i]-3:y_end_list[i]+1+3,:]
+            
+            line_color = data_flt[y_start_list[i]-3:y_end_list[i]+1+3,:]
+            line_data_plan_arrive = \
+            data_plan_arrive[y_start_list[i]-3:y_end_list[i]+1+3,:]
+            line_merge = np.hstack((line_color, line_data_plan_arrive))
+            cv2.imwrite('line/'+ img_name+ '_line_' + str(i) + '.bmp', line_merge)
+            
+            
+            for j in range(line_flt_number_start, line_flt_number_end + 1,character_width+1):
+                Character = line[:, j: j+character_width+1]
+                #cv2.imshow("Character_1", Character_1)
+                #cv2.waitKey(0)
+                if Character.sum()<>0:
+                    cv2.imwrite('character/'+ img_name+ '_line_' + str(i) + '_' + str(j) + 'flt.tif', Character)
+                    character_list.append(Character.tolist())
+                    
+            for j in range(line_plane_number_start, line_plane_number_end + 1,character_width+1):
+                Character = line[:, j: j+character_width+1]
+                #cv2.imshow("Character_1", Character_1)
+                #cv2.waitKey(0)
+                if Character.sum()<>0:
+                    cv2.imwrite('character/'+ img_name+ '_line_' + str(i) + '_' + str(j) + 'plane.tif', Character)
+                    character_list.append(Character.tolist())
+                    
+            for j in range(line_stand_start, line_stand_end + 1,character_width+1):
+                Character = line[:, j: j+character_width+1]
+                #cv2.imshow("Character_1", Character_1)
+                #cv2.waitKey(0)
+                if Character.sum()<>0:
+                    cv2.imwrite('character/'+ img_name+ '_line_' + str(i) + '_' + str(j) + 'stand.tif', Character)
+                    character_list.append(Character.tolist())
+            
+    
+    character_series = pd.Series(character_list)
+    no_repeat = character_series.value_counts()
+    for number,item in enumerate(no_repeat.index):
+        np_item = np.array(item)        
+        cv2.imwrite('no_repeat_character/' + \
+        str(no_repeat.iloc[number]) + '_' + \
+        str(number) + '.tif', np_item)
         
         
         
