44c6b20e27ec8e8abf355b1a13d34a858340fc8b
xsl <469798015@qq.com>
2018-04-04 15:46:12 +0800

    生成中文字符训练集

--- character library/chinese library.py
+++ character library/chinese library.py
@@ -8,6 +8,7 @@ Created on Wed Apr 04 06:25:02 2018
 #encoding: utf-8
 import os
 import pygame
+import cv2
 
 chinese_dir = 'chinese'
 if not os.path.exists(chinese_dir):
@@ -15,14 +16,18 @@ if not os.path.exists(chinese_dir):
 
 pygame.init()
 start,end = (0x30,0x5b) # 大写字母编码范围
-for codepoint in range(30,126+1):
+for codepoint in range(33,126+1):
     word = unichr(codepoint)
     font = pygame.font.Font("simsun.ttc", 13)
     # 当前目录下要有微软雅黑的字体文件msyh.ttc,或者去c:\Windows\Fonts目录下找
     # 64是生成汉字的字体大小
     rtext = font.render(word, False, (0, 0, 0), (255, 255, 255))
     pygame.image.save(rtext, os.path.join(chinese_dir, str(codepoint) + ".png"))
+    img_dir = os.path.join(chinese_dir, str(codepoint) + ".png")
+    img = cv2.imread(img_dir, 0)
+    ret,thresh_img = cv2.threshold(img,160,255,cv2.THRESH_BINARY_INV)
+    cv2.imwrite(img_dir, thresh_img)
 
-word = u'航'   
-rtext = font.render(word, False, (0, 0, 0), (255, 255, 255))
-pygame.image.save(rtext, os.path.join(chinese_dir, word + ".png"))
+#word = u'航'   
+#rtext = font.render(word, False, (0, 0, 0), (255, 255, 255))
+#pygame.image.save(rtext, os.path.join(chinese_dir, word + ".png"))
